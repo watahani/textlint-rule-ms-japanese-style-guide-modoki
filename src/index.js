624497b18897c01605d4d4e7cba24c9b01a12705
Wataru Haniyama <haniyama.wataru+work@gmail.com>
2019-03-28 20:08:02 +0900

    add space rule

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 "use strict";
 const chouon = require('./chouon')
 
+const checkNoSpace = (text) => {
+    const noSpace_before = /[^a-zA-Z0-9!-\/:-@¥[-`{-~\s]([a-zA-Z0-9!-\/:-@¥[-`{-~])+?/gm
+    const noSpace_after = /[a-zA-Z0-9!-\/:-@¥[-`{-~]+?[^a-zA-Z0-9!-\/:-@¥[-`{-~\s]/gm
+        
+    const matches_b = noSpace_before.exec(text)
+    const matches_a = noSpace_after.exec(text)
+    let results = []
+    if(matches_b) {
+        results.push({
+            message: "スペースが必要です",
+            index: matches_b.index
+        })
+    }
+    if(matches_a) {
+        results.push({
+            message: "スペースが必要です",
+            index: matches_a.index
+        })
+    }
+    return results
+}
+
 module.exports = function(context, options = {}) {
     const {Syntax, RuleError, report, getSource} = context;
     return {
@@ -17,6 +39,12 @@ module.exports = function(context, options = {}) {
                 });
                 report(node, ruleError);                
             });
+            
+            const results = checkNoSpace(text)
+            results.forEach( r => {
+                const ruleError = new RuleError(r.message, {index: r.index})
+                report(node, ruleError)
+            })
         }
     }
 };
